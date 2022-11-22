4b2fdae5dc1b297d0cecf67e62537fdad76e8de4
Sergio Omar Sánchez <sanchez.omar.sergio@gmail.com>
2022-11-22 19:50:51 -0300

    card section and cards created

---
+++ pizzaapp/component/PizzaCard.jsx
@@ -0,0 +1,18 @@
+import Image from 'next/image'
+import React from 'react'
+import style from '../styles/PizzaCard.module.css'
+function PizzaCard() {
+  return (
+    <div className={style.container}>
+        <Image src='/img/pizza.png' alt='' width='200' height='200'/>
+        <h1 className={style.title}>FIORI DI ZUCCA</h1>
+        <span className={style.price}>$19.90</span>
+        <p className={style.desc}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </p>
+
+    </div>
+  )
+}
+
+export default PizzaCard
---
+++ pizzaapp/component/PizzaList.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import style from '../styles/PizzaList.module.css'
+import PizzaCard from './PizzaCard'
+
+
+function PizzaList() {
+  return (
+    <div className={style.container}>
+        <h1 className={style.title}>The best Pizza in Town</h1>
+        <p className={style.desc}>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+         sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+         nisi ut aliquip ex ea commodo consequat.</p>
+        <div className={style.wrapper}>
+        <PizzaCard/>
+        <PizzaCard/>
+        <PizzaCard/>
+        <PizzaCard/>
+        <PizzaCard/>
+        <PizzaCard/>
+        <PizzaCard/>
+        <PizzaCard/>
+        </div>
+    </div>
+  )
+}
+
+export default PizzaList
